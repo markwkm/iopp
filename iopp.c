d55c3a7bf9e3e69b5d377593122011495ed1b59b
Mark Wong <markwkm@gmail.com>
2008-10-22 20:01:50 -0700

    Show the difference in stats between samples by implementing a simple linked list data structure.

--- iopp.c
+++ iopp.c
@@ -24,13 +24,9 @@
 		value[length] = '\0'; \
 		v = atoll(value);
 
-void
-get_stats()
+struct io_node
 {
-	DIR *dir = opendir(PROC);
-	struct dirent *ent;
-	char filename[64];
-	char buffer[256];
+	int pid;
 	long long rchar;
 	long long wchar;
 	long long syscr;
@@ -39,6 +35,72 @@ get_stats()
 	long long write_bytes;
 	long long cancelled_write_bytes;
 	char command[64];
+	struct io_node *next;
+};
+
+struct io_node *head = NULL;
+
+/* Prototypes */
+struct io_node *get_ion(int);
+struct io_node *new_ion(char *);
+void upsert_data(struct io_node *);
+
+struct io_node *
+get_ion(int pid)
+{
+	struct io_node *c = head;
+
+	while (c != NULL)
+	{
+		if (c->pid == pid)
+			break;
+		c = c->next;
+	}
+	return c;
+}
+
+struct io_node *
+insert_ion(struct io_node *ion)
+{
+	struct io_node *c;
+	struct io_node *p;
+
+	/* Check the head of the list as a special case. */
+	if (ion->pid < head->pid)
+	{
+		ion->next = head;
+		head = ion;
+		return head;
+	}
+
+	c = head->next;
+	p = head;
+	while (c != NULL)
+	{
+		if (ion->pid < c->pid)
+		{
+			ion->next = c;
+			p->next = ion;
+			return head;
+		}
+		p = c;
+		c = c->next;
+	}
+
+	/* Append to the end of the list. */
+	if (c == NULL)
+		p->next = ion;
+
+	return head;
+}
+
+void
+get_stats()
+{
+	DIR *dir = opendir(PROC);
+	struct dirent *ent;
+	char filename[64];
+	char buffer[256];
 
 	char value[64];
 
@@ -54,14 +116,22 @@ get_stats()
 		char *p;
 		char *q;
 
+		struct io_node *ion;
+		struct io_node *old_ion;
+
 		if (!isdigit(ent->d_name[0]))
 			continue;
+
+		ion = new_ion(ent->d_name);
 
 		/* Read 'io' file. */
 		sprintf(filename, "%s/%s/stat", PROC, ent->d_name);
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
+		{
+			free(ion);
 			continue;
+		}
 		/*
 		 * The command is near the beginning; we don't need to be able to
 		 * the entire stat file.
@@ -73,34 +143,100 @@ get_stats()
 		++p;
 		q = strchr(p, ')');
 		length = q - p;
-		strncpy(command, p, length);
-		command[length] = '\0';
+		strncpy(ion->command, p, length);
+		ion->command[length] = '\0';
 
 		/* Read 'io' file. */
 		sprintf(filename, "%s/%s/io", PROC, ent->d_name);
 		fd = open(filename, O_RDONLY);
 		if (fd == -1)
+		{
+			free(ion);
 			continue;
+		}
 		length = read(fd, buffer, sizeof(buffer) - 1);
 		close(fd);
 		buffer[length] = '\0';
 
 		/* Parsing the io file data. */
 		p = buffer;
-		GET_VALUE(rchar);
-		GET_VALUE(wchar);
-		GET_VALUE(syscr);
-		GET_VALUE(syscw);
-		GET_VALUE(read_bytes);
-		GET_VALUE(write_bytes);
-		GET_VALUE(cancelled_write_bytes);
+		GET_VALUE(ion->rchar);
+		GET_VALUE(ion->wchar);
+		GET_VALUE(ion->syscr);
+		GET_VALUE(ion->syscw);
+		GET_VALUE(ion->read_bytes);
+		GET_VALUE(ion->write_bytes);
+		GET_VALUE(ion->cancelled_write_bytes);
+
+		old_ion = get_ion(ion->pid);
 
 		/* Display the pid's io data. */
-		printf("%5s %8lld %8lld %8lld %8lld %8lld %8lld %8lld %s\n",
-				ent->d_name, rchar, wchar, syscr, syscw, read_bytes,
-				write_bytes, cancelled_write_bytes, command);
+		if (old_ion != NULL)
+			printf("%5d %8lld %8lld %8lld %8lld %8lld %8lld %8lld %s\n",
+					ion->pid,
+					ion->rchar - old_ion->rchar,
+					ion->wchar - old_ion->wchar,
+					ion->syscr - old_ion->syscr,
+					ion->syscw - old_ion->syscw,
+					ion->read_bytes - old_ion->read_bytes,
+					ion->write_bytes - old_ion->write_bytes,
+					ion->cancelled_write_bytes - old_ion->cancelled_write_bytes,
+					ion->command);
+		else
+			/* No previous data, show 0's instead of calculating negatives. */
+			printf("%5d %8d %8d %8d %8d %8d %8d %8d %s\n",
+					ion->pid, 0, 0, 0, 0, 0, 0, 0, ion->command);
+
+		upsert_data(ion);
 	}
 	closedir(dir);
+	return;
+}
+
+struct io_node *
+new_ion(char *pid)
+{
+	struct io_node *ion;
+
+	ion = (struct io_node *) malloc(sizeof(struct io_node));
+	bzero(ion, sizeof(struct io_node));
+	ion->pid = atoi(pid);
+
+	return ion;
+}
+
+void
+upsert_data(struct io_node *ion)
+{
+	struct io_node *n;
+
+	/* List is empty. */
+	if (head == NULL)
+	{
+		head = ion;
+		return;
+	}
+
+	/* Check if we have seen this pid before. */
+	n = head;
+	while (n != NULL)
+	{
+		if (n->pid == ion->pid)
+		{
+			n->rchar = ion->rchar;
+			n->wchar = ion->wchar;
+			n->syscr = ion->syscr;
+			n->syscw = ion->syscw;
+			n->read_bytes = ion->read_bytes;
+			n->write_bytes = ion->write_bytes;
+			n->cancelled_write_bytes = ion->cancelled_write_bytes;
+			return;
+		}
+		n = n->next;
+	}
+
+	/* Add this pid to the list. */
+	head = insert_ion(ion);
 	return;
 }
 
